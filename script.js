73b9d533e3202c08a2a680914c29e2d1b910d9ff
mnap <learn@10g.pl>
2018-04-03 23:20:04 +0200

    Update - code refactoring

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+var movies = [
+    {
+        title: 'Harry Potter',
+        desc: 'A movie about a wizard'
+    },
+    {
+        title: 'The Lion King',
+        desc: 'A movie about a king of the Pride Lands'
+    }
+];
+
+var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc)
+    );
+});
+
 var element =
     React.createElement('div', {},
         React.createElement('h1', {}, 'Private Movie Database'),
-        React.createElement('ul', {},
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'Harry Potter'),
-                React.createElement('p', {}, 'A movie about a wizard')
-            ),
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'The Lion King'),
-                React.createElement('p', {},
-                    'A movie about a king of the Pride Lands')
-            )
-        )
+        React.createElement('ul', {}, moviesElements)
     );
 
 ReactDOM.render(element, document.getElementById('app'));
