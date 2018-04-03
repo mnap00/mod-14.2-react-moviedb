bd9f9783437aacd2be93d6032ae762798fdc06f0
mnap <learn@10g.pl>
2018-04-04 00:00:50 +0200

    Add images and two more array elements

--- script.js
+++ script.js
@@ -2,19 +2,34 @@ var movies = [
     {
         id: 1,
         title: 'Harry Potter',
-        desc: 'A movie about a wizard'
+        desc: 'A movie about a wizard',
+        img: 'images/harry-potter.jpg'
     },
     {
         id: 2,
         title: 'The Lion King',
-        desc: 'A movie about a king of the Pride Lands'
+        desc: 'A movie about a king of the Pride Lands',
+        img: 'images/lion-king.jpg'
+    },
+    {
+        id: 3,
+        title: 'Star Wars',
+        desc: 'Fantasy story in space',
+        img: 'images/star-wars.jpg'
+    },
+    {
+        id: 4,
+        title: 'Raiders of the Lost Ark',
+        desc: 'Adventures of Indiana Jones - the archeologist',
+        img: 'images/indiana-jones.jpg'
     }
 ];
 
 var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.img})
     );
 });
 
