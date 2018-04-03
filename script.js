00f6ce53d49d757395d125d7b6ac6a097c057ade
mnap <learn@10g.pl>
2018-04-03 23:06:25 +0200

    Update ReactElement with more complicated example

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var element = React.createElement('div', {}, 'Hello world!');
+var element =
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Private Movie Database'),
+        React.createElement('ul', {},
+            React.createElement('li', {},
+                React.createElement('h2', {}, 'Harry Potter'),
+                React.createElement('p', {}, 'A movie about a wizard')
+            ),
+            React.createElement('li', {},
+                React.createElement('h2', {}, 'The Lion King'),
+                React.createElement('p', {},
+                    'A movie about a king of the Pride Lands')
+            )
+        )
+    );
+
 ReactDOM.render(element, document.getElementById('app'));
