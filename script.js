4ee94458be822c66b6312aaebaf488d537132008
mnap <learn@10g.pl>
2018-04-03 23:22:47 +0200

    Update - add id key

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var movies = [
     {
+        id: 1,
         title: 'Harry Potter',
         desc: 'A movie about a wizard'
     },
     {
+        id: 2,
         title: 'The Lion King',
         desc: 'A movie about a king of the Pride Lands'
     }
 ];
 
 var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {},
+    return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc)
     );
